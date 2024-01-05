27505953e7eedf3283d59e7dd1dabba3e1a34907
kasapvictor <kasap.victor@gmail.com>
2024-01-05 14:58:20 -0800

    feat: Added fibonacci

---
+++ src/fibonacci.ts
@@ -0,0 +1,10 @@
+const fibonacci = (n: number): number => {
+  if (n < 2) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.log(fibonacci(5));
+console.log(fibonacci(50));
---
+++ src/fibonacci2.ts
@@ -0,0 +1,14 @@
+const fibonacci2 = (n: number, memo: Record<number, number> = {}): number => {
+  if (memo[n] !== undefined) {
+    return memo[n];
+  }
+
+  if (n <= 2) {
+    return 1;
+  }
+
+  return (memo[n] = fibonacci2(n - 1, memo) + fibonacci2(n - 2, memo));
+};
+
+console.log(fibonacci2(5));
+console.log(fibonacci2(50));
