36dd92ead33cd90e189139d95cab36149ce302b7
kasapvictor <kasap.victor@gmail.com>
2024-01-06 06:50:48 -0800

    feat: Added throttling

---
+++ src/throttling.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-this-alias, @typescript-eslint/strict-boolean-expressions */
+
+function throttle(func, limit) {
+  let inThrottle;
+
+  return function () {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+// Пример использования
+window.addEventListener(
+  'scroll',
+  throttle(() => {
+    console.log('Scroll event handler call at most every 100ms');
+  }, 100),
+);
