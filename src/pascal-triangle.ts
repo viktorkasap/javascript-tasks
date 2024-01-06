b5128fbfc235118ec4b440de14c5ec7d2362b01e
kasapvictor <kasap.victor@gmail.com>
2024-01-06 06:01:59 -0800

    feat: Added Pascal Triangle

---
+++ src/pascal-triangle.ts
@@ -0,0 +1,19 @@
+const pascalTriangle = (rows: number): number[][] => {
+  const triangle = [[1], [1, 1]];
+
+  for (let i = 2; i < rows; i += 1) {
+    const row = [1];
+
+    for (let j = 1; j < i; j += 1) {
+      row[j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
+    }
+
+    row.push(1);
+    triangle.push(row);
+  }
+
+  return triangle;
+};
+
+console.log(pascalTriangle(3));
+console.log(pascalTriangle(5));
