c6a242127dcb2d20b789b88c7595c85ea24e2efd
kasapvictor <kasap.victor@gmail.com>
2024-02-19 16:29:12 -0800

    feat: Added Shuffle Array2

--- src/shuffleArray.ts
+++ src/shuffleArray.ts
@@ -1,9 +1,24 @@
+/** Version 1/
+// const shuffle = (array: number[]): number[] => {
+//   return [...array].sort(() => Math.random() - 0.5);
+// };
+
+
+
+/* Version 2 */
 const shuffle = (array: number[]): number[] => {
-  return [...array].sort(() => Math.random() - 0.5);
+  const elements = [...array];
+
+  for (let i = elements.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+
+    [elements[i], elements[j]] = [elements[j], elements[i]];
+  }
+
+  return elements;
 };
 
 const arr = [1, 2, 3];
-
 console.log([1], shuffle(arr));
 console.log([2], shuffle(arr));
 console.log([3], shuffle(arr));
