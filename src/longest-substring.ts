5d17333b441691f44c39ea5c6d531b58881c1922
kasapvictor <kasap.victor@gmail.com>
2024-04-04 19:48:19 -0700

    feat: Added longest substring

---
+++ src/longest-substring.ts
@@ -0,0 +1,32 @@
+const longestSubstring = (str: string): number => {
+  const substr = new Set();
+  let position = 0;
+  let maxLength = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (!substr.has(str[i])) {
+      substr.add(str[i]);
+    } else {
+      while (substr.has(str[position])) {
+        substr.delete(str[position]);
+        position += 1;
+      }
+      substr.add(str[i]);
+    }
+
+    if (maxLength < [...substr].length) {
+      maxLength = [...substr].length;
+    }
+  }
+
+  return maxLength;
+};
+
+console.log('abcabcbb', { expect: 3, result: longestSubstring('abcabcbb') });
+console.log('bbbbb', { expect: 1, result: longestSubstring('bbbbb') });
+console.log('pwwkew', { expect: 3, result: longestSubstring('pwwkew') });
+
+/**
+ * TERMINAL ->
+ * clear && npx ts-node src/longest-substring.ts
+ */
